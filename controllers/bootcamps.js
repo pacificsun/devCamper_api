22ca15d7b7d4faa055aeb0d8c0df82ab8405ca4a
Suraj Thapa <stsurajst@gmail.com>
2020-03-12 17:36:51 +0545

    route setup

---
+++ controllers/bootcamps.js
@@ -0,0 +1,42 @@
+//@desc Gel all bootcamps
+//@route GET /api/v1/bootcamps
+//@access Public
+
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+};
+
+//@desc Get single bootcamps
+//@route GET /api/v1/bootcamps/:id
+//@access Public
+
+exports.getBootcamp = (req, res, next) => {
+  res.status(200).json({ sucess: true, msg: `get bootcamp ${req.params.id}` });
+};
+
+//@desc Create a new bootcamps
+//@route POST /api/v1/bootcamps
+//@access Private
+
+exports.createBootcamp = (req, res, next) => {
+  res.status(200).json({ sucess: true, msg: 'Create new bootcamp' });
+};
+
+//@desc Update a bootcamps
+//@route PUT /api/v1/bootcamps/:id
+//@access Private
+
+exports.updateBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ sucess: true, msg: `Update bootcamp ${req.params.id}` });
+};
+//@desc Delete a bootcamps
+//@route DELETE /api/v1/bootcamps/:id
+//@access Private
+
+exports.deleteBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ sucess: true, msg: `Delete bootcamp ${req.params.id}` });
+};
