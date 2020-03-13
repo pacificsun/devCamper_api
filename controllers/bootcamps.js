7db699e2b8e34127b7f74821c368f916e33987f8
Suraj Thapa <stsurajst@gmail.com>
2020-03-13 13:23:38 +0545

    colors added, mongodb is connected and handle unhandled promise rejection

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,7 +3,9 @@
 //@access Public
 
 exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+  res
+    .status(200)
+    .json({ success: true, msg: 'Show all bootcamps', hello: req.hello });
 };
 
 //@desc Get single bootcamps
--- server.js
+++ server.js
@@ -1,20 +1,41 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-// Route files
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
+const colors = require('colors');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+
+const logger = (req, res, next) => {
+  req.hello = 'hello world';
+  console.log('Middleware ran');
+  next();
+};
+app.use(logger);
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
-app.listen(
+
+const server = app.listen(
   PORT,
   console.log(
-    `server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    `server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
   )
 );
+
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
