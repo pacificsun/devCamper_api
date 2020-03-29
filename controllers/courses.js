84b006c98d74aec27f6b1da0668c661fb2d10a00
Suraj Thapa <stsurajst@gmail.com>
2020-03-29 15:29:07 +0545

    delete course

--- controllers/courses.js
+++ controllers/courses.js
@@ -67,3 +67,44 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
     data: course
   });
 });
+
+// @desc Update course
+// @route PUT /api/v1/courses/:id
+// @access Private
+
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+  let course = await Course.findById(req.params.id);
+  if (!course) {
+    return next(
+      new ErrorResponse(`No Course with the id of ${req.params.id}`, 404)
+    );
+  }
+
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    success: true,
+    data: course
+  });
+});
+
+// @desc Delete Course\
+// @route DELETE /api/v1/courses/:id
+// @access Private
+
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+  console.log('res.params.id>>', req.params);
+  const course = await Course.findById(req.params.id);
+
+  if (!course) {
+    next(new ErrorResponse(`No course with the id of ${req.params.id}`, 404));
+  }
+  await course.remove();
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+});
